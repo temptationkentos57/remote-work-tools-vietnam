216ab13b75906314c71d55ea18e272be77d26c6c
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-28 15:36:24 +0200

    Fix welcome message formatting for improved readability.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Remote Work Tool application! / Chào mừng đến với ứng dụng Công cụ Làm việc từ xa!');
+    res.send('Welcome to the Remote Work Tool application!\nChào mừng đến với ứng dụng Công cụ Làm việc từ xa!');
 });
 
 app.listen(PORT, () => {
