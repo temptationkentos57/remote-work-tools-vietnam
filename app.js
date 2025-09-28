14fbc62ed4b753e5ce025f069206d748a5d76d78
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-28 15:36:12 +0200

    Fix typos in console log message and route response

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server đang chạy trên http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
